de2629b2df09fbcdd499e59012c74360300cdd22
Zippe <ingrid_zippe@brown.edu>
2018-10-16 21:25:07 -0700

    form is set up

---
+++ app.js
@@ -0,0 +1,40 @@
+"use strict";
+
+var express = require('express');
+var app = express();
+var path = require('path');
+const https = require('https');
+const http = require('http');
+
+// function to poke the heroku server
+// setInterval(function() {
+//     http.request('https://greve-chocolatine-34680.herokuapp.com/', console.log("server poked")).end();
+// }, 300000); // every 5 minutes (300000)
+
+var exphbs = require('express-handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', function(req, res) {
+  res.render('index')
+});
+
+app.get('/generatepresave', function(req, res) {
+  console.log(req.query.title);
+  console.log(req.query.description);
+  console.log(req.query.copy);
+  console.log(req.query.spotifyartistid);
+  console.log(req.query.spotifyuri);
+  console.log(req.query.appleid);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port);
+console.log('Express started. Listening on port %s', port);
